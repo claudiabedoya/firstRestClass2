b04689ea7cdcf2bba689121ad3eb8b4bec758166
microservicios <microservicios@PC-LIS>
2019-05-04 12:08:53 -0500

    5464456

--- src/main/java/com/multiplication/rest/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/com/multiplication/rest/multiplication/controller/MultiplicationResultAttemptController.java
@@ -3,11 +3,16 @@ package com.multiplication.rest.multiplication.controller;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.multiplication.rest.multiplication.domain.MultiplicationResultAttempt;
@@ -27,11 +32,31 @@ final class MultiplicationResultAttemptController {
         this.multiplicationService = multiplicationService;
     }
 
+    /*
     @PostMapping
     ResponseEntity<ResultResponse> postResult(@RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
         return ResponseEntity.ok(
                 new ResultResponse(multiplicationService
                         .checkAttempt(multiplicationResultAttempt)));
+    }*/
+    
+    @PostMapping
+    ResponseEntity<MultiplicationResultAttempt> postResult(@RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
+        boolean isCorrect = multiplicationService.checkAttempt(multiplicationResultAttempt);
+        MultiplicationResultAttempt attemptCopy = new MultiplicationResultAttempt(
+                multiplicationResultAttempt.getUser(),
+                multiplicationResultAttempt.getMultiplication(),
+                multiplicationResultAttempt.getResultAttempt(),
+                isCorrect
+        );
+        return ResponseEntity.ok(attemptCopy);
+    }
+    
+    @GetMapping
+    ResponseEntity<List<MultiplicationResultAttempt>> getStatistics(@RequestParam("alias") String alias) {
+        return ResponseEntity.ok(
+                multiplicationService.getStatsForUser(alias)
+        );
     }
 
     @RequiredArgsConstructor
